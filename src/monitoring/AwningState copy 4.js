c2ebade8e21a184db5a3cc604b9fc9a4fea78d40
inayong <141033984+inayong@users.noreply.github.com>
2023-12-30 20:06:00 +0900

    12.30
    - 체크박스 전체 및 개별 선택 기능 완료(페이지 이동 시 선택 유지 가능)
    - 체크박스 선택 개수 표시 완료
    - 필터링 및 체크박스 초기화 완료
    - 상태별 색상 구분 완료

--- src/monitoring/AwningState copy 4.js
+++ src/monitoring/AwningState copy 4.js
@@ -16,20 +16,8 @@ const TableHead = ({ title, className, rowSpan, colSpan }) => {
 };
 
 const TableBody = ({ content, className, scope, deviceId }) => {
-  let statusColor = 'text-gray-500';
-  // if (content === '정상' || content === '연결' || content === '켜짐' || content === '열림') {
-  //   statusColor = 'text-green-500';
-  // } else {
-  //   statusColor = 'text-gray-500';
-  // }
-  if (['정상', '연결', '켜짐', '열림'].includes(content)) {
-    statusColor = 'text-green-500'; // 긍정적 상태일 때
-  } else if (['고장', '끊김', '꺼짐', '닫힘'].includes(content)) {
-    statusColor = 'text-red-500'; // 부정적 상태일 때
-  }
-
   return (
-    <td className={`px-6 py-2 ${className || ''} ${statusColor}`} scope={scope || undefined} >
+    <td className={`px-6 py-2 ${className || ''}`} scope={scope || undefined} >
       {content === "상세" ? (
         <Link to={`detail/${deviceId}`}>
           <button className='border p-1 rounded-full text-xs text-black shadow-sm'>상세</button>
@@ -47,29 +35,18 @@ const AwningState = () => {
   const [getStateData, setGetStateData] = useState([]); //데이터
   const [stateData, setStateData] = useState([]); //페이징 데이터
   const [isOpen, setIsOpen] = useState(true);
-  const [checkbox, setCheckBox] = useState({});
-  // const [checkbox, setCheckBox] = useState(new Map());
-  const [checkboxCount, setCheckboxCount] = useState(0);
+  const [checkbox, setCheckBox] = useState([]);
   const [showModal, setShowModal] = useState(false);
-  const [isFilter, setIsFilter] = useState(false);
 
   const searchSel = useRef();
   const searchKeyword = useRef();
-  const filterSel = useRef();
 
-
-  const handleSearchbutton = () => {
-    console.log("input")
-  }
-  
   const handleIsOpen = () => {
     setIsOpen(!isOpen);
   }
 
-  const filterResetButton = () => {
-    searchSel.current.value = '';
-    searchKeyword.current.value = '';
-    filterSel.current.value = '';
+  const handlebutton = () => {
+    console.log("input")
   }
 
   useEffect(() => {
@@ -84,12 +61,7 @@ const AwningState = () => {
       })
         .then(resp => resp.json())
         .then(data => {
-          setGetStateData(data);
-          const checkedState = data.reduce((acc, item) => {
-            acc[item.deviceId] = false;
-            return acc;
-          }, {});
-          setCheckBox(checkedState);
+          setGetStateData(data)
           console.log("statedata", data)
         })
         .catch(err => console.error(err))
@@ -110,53 +82,37 @@ const AwningState = () => {
   const handlePageChange = (pageNum) => {
     setPage(pageNum);
     console.log("pageNum", pageNum)
-    // setCheckBox([]);
   }
 
   //checkbox 단일 선택
   const handleSingleCheck = (checked, deviceId) => {
-    // setCheckBox(prev => {
-    //   const newChecked = new Map(prev);
-    //   newChecked.set(deviceId, checked);
-    //   //선택된 항목 수 업데이트
-    //   setCheckboxCount(Array.from(newChecked.values()).filter(val => val).length);
-    //   return newChecked;
-    // });
-    setCheckBox(prev => ({ ...prev, [deviceId]: checked }));
+    if (checked) {
+      setCheckBox(checkbox => [...checkbox, deviceId]); //불변성을 지키기 위한 원본 배열을 복사 후 추가
+      // console.log("single checked", [...checkbox, deviceId]);
+    } else {
+      setCheckBox(checkbox => checkbox.filter(item => item !== deviceId)) //단일 선택 해제 시 체크된 아이템을 제외한 배열
+      // console.log("single", checkbox.filter(item => item !== deviceId));
+    }
   };
 
 
   //checkbox 전체 선택
   const handleAllCheck = (checked) => {
-    // const newChecked = new Map();
-    // stateData.forEach(items => {
-    //   newChecked.set(items.deviceId, checked);
-    // });
-    // setCheckBox(newChecked);
-    // //선택 수 업데이트
-    // setCheckboxCount(checked ? stateData.length : 0);
-    const newChecked = { ...checkbox };
-    stateData.forEach(items => {
-      newChecked[items.deviceId] = checked;
-    });
-    setCheckBox(newChecked);
+    if (checked) {
+      const idArray = stateData.map(item => item.deviceId);
+      setCheckBox(idArray);
+      // console.log("all", idArray)
+    } else {
+      setCheckBox([]);
+      // console.log("all[]")
+    }
   };
-
-  //체크박스 개수
-  useEffect(() => {
-    const count = Object.values(checkbox).filter(checked => checked).length;
-    setCheckboxCount(count);
-  }, [checkbox])
-
-  const checkboxResetButton = () => {
-    setCheckBox({});
-    setCheckboxCount(0);
-  }
 
   //checkbox선택시 삭제 버튼 나타내기
   const checkboxAwningDelete = () => {
     console.log("선택된 항목 삭제:", checkbox);
     setShowModal(true);
+
 
   }
 
@@ -192,7 +148,7 @@ const AwningState = () => {
         </div>
       )} */}
       <div className={`flex justify-center pt-14 ${isOpen ? 'h-[28%]' : 'h-32'} `}>
-        <div className='bg-white w-5/6 rounded-lg shadow-md'>
+        <div className='bg-white w-5/6'>
           <div className='flex justify-between'>
             <div className='flex p-3'>
               <div className='flex items-center w-48'>
@@ -200,17 +156,17 @@ const AwningState = () => {
                 <div className='text-lg'>검색</div>
               </div>
               <select ref={searchSel} className='bg-slate-200 w-36 pl-2 rounded-lg'>
-                <option value=''>전체</option>
+                <option value='전체'>전체</option>
                 <option value='관리번호'>관리번호</option>
                 <option value='설치장소'>설치장소</option>
               </select>
               <div className='flex bg-slate-300 ml-2 items-center rounded-lg'>
                 <input type='text' ref={searchKeyword} className='w-72 bg-slate-300' />
-                <button onClick={handleSearchbutton} className='absolute ml-[260px]'><RiSearch2Line /></button>
+                <button onClick={handlebutton} className='absolute ml-[260px]'><RiSearch2Line /></button>
               </div>
             </div>
             <div className='flex pr-3'>
-              <button onClick={filterResetButton} className='flex bg-gray-100 items-center mx-4 my-3 px-2 rounded-lg hover:bg-gray-200'>
+              <button className='flex bg-gray-100 items-center mx-4 my-3 px-2 rounded-lg hover:bg-gray-200'>
                 <div className='pr-2'>초기화</div>
                 <GrPowerReset />
               </button>
@@ -226,10 +182,8 @@ const AwningState = () => {
                   <RiFilter3Line size={20} className='m-3 fill-gray-500' />
                   <div className='text-lg'>필터</div>
                 </div>
-                <select ref={filterSel} className='w-36 pl-2 rounded-lg'>
+                <select className='w-36 pl-2 rounded-lg'>
                   <option value=''>전체</option>
-                  <option value='통신'>통신</option>
-                  <option value='어닝'>어닝</option>
                 </select>
               </div>
               <div className='flex p-3'>
@@ -237,7 +191,7 @@ const AwningState = () => {
                   <RiCheckboxLine size={20} className='m-3 fill-gray-500' />
                   <div className='text-lg'>장치 관리</div>
                 </div>
-                {checkboxCount > 0 && (
+                {checkbox.length > 0 && (
                   <button onClick={checkboxAwningDelete} className='hover:bg-red-100 border-4 border-red-400 rounded-lg px-2 mr-2'>어닝삭제</button>
                 )}
                 <button className='hover:bg-gray-200 border rounded-lg px-2 mx-2'>조명 on</button>
@@ -253,21 +207,16 @@ const AwningState = () => {
           )}
         </div>
       </div>
-      <div className='flex justify-center mt-14'>
-        <div className='w-5/6 p-4 text-xl flex'>
-          <span>총 <span className='font-bold'>7</span>건 {checkboxCount > 0 && (<span>중 <span className='font-bold'>{checkboxCount}</span>건 선택</span>)}</span>
-          {checkboxCount > 0 && (<button onClick={checkboxResetButton} className='ml-6 text-base items-center'><GrPowerReset /></button>)}
-        </div>
-      </div>
-      <div className='flex justify-center h-1/2'>
+      <div className='flex justify-center h-1/2 mt-20'>
+          <div className=''>d</div>
         <div className='bg-white w-5/6 h-full flex flex-col shadow-md sm:rounded-lg'>
-          <div className="flex-grow overflow-x-auto max-h-full hide-scrollbar rounded-t-lg">
+          <div className="flex-grow relative overflow-x-auto hide-scrollbar">
             <table className="min-w-full text-center text-gray-500 whitespace-nowrap">
-              <thead className="text-sm text-gray-700 bg-gray-50">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                   <th scope="col" className="p-4" rowSpan="2">
                     <div className="flex items-center">
-                      <input id="checkbox-all" type="checkbox" onChange={(e) => handleAllCheck(e.target.checked)} checked={stateData.every(items => checkbox[items.deviceId])} className="w-4 h-4 rounded focus:ring-blue-500 focus:ring-2" />
+                      <input id="checkbox-all" type="checkbox" onChange={(e) => handleAllCheck(e.target.checked)} checked={checkbox.length === stateData.length ? true : false} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                       <label htmlFor="checkbox-all" className="sr-only">checkbox</label>
                     </div>
                   </th>
@@ -293,20 +242,20 @@ const AwningState = () => {
                   <TableHead title="배터리" />
                 </tr>
               </thead>
-              <tbody className='text-sm'>
+              <tbody>
                 {stateData.map((items, index) => (
-                  <tr key={index} className={`${checkbox[items.deviceId] ? 'bg-blue-50' : 'bg-white border-b hover:bg-gray-50'}`}>
+                  <tr key={index} className="bg-white border-b hover:bg-gray-50 ">
                     <td className="w-4 p-4">
                       <div className="flex items-center">
-                        <input id={`checkbox-${index}`} type="checkbox" name={`select-${items.deviceId}`} onChange={(e) => handleSingleCheck(e.target.checked, items.deviceId)} checked={checkbox[items.deviceId] || false} className="w-4 h-4 rounded focus:ring-blue-500  focus:ring-2 " />
+                        <input id={`checkbox-${index}`} type="checkbox" name={`select-${items.deviceId}`} onChange={(e) => handleSingleCheck(e.target.checked, items.deviceId)} checked={checkbox.includes(items.deviceId) ? true : false} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500  focus:ring-2 " />
                         <label htmlFor="checkbox-single" className="sr-only">checkbox</label>
                       </div>
                     </td>
                     <TableBody scope="row" content="상세" deviceId={items.deviceId} />
                     <TableBody content={items.managementNumber} />
-                    <TableBody content={items.statusConnected === 'on' ? '연결' : '끊김'} />
-                    <TableBody content={items.statusLighting === 'on' ? '켜짐' : '꺼짐'} />
-                    <TableBody content={items.statusAwningExpand === 'on' ? '열림' : '닫힘'} />
+                    <TableBody content={items.statusConnected} />
+                    <TableBody content={items.statusLighting} />
+                    <TableBody content={items.statusAwningExpand} />
                     <TableBody content={items.statusOperationMode} />
                     <TableBody content={items.statusBatteryCharge} />
                     <TableBody content={`${items.managementArea1} ${items.managementArea2}`} />
@@ -324,9 +273,9 @@ const AwningState = () => {
               </tbody>
             </table>
           </div>
-          <div className='relative bottom-3'>
-            <Pagination activePage={page} itemsCountPerPage={perPage} totalItemsCount={getStateData.length} pageRangeDisplayed={3} onChange={handlePageChange} />
-          </div>
+            <div className='relative bottom-3'>
+              <Pagination activePage={page} itemsCountPerPage={perPage} totalItemsCount={getStateData.length} pageRangeDisplayed={3} onChange={handlePageChange} />
+            </div>
         </div>
       </div>
     </div>
